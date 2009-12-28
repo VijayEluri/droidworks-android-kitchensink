a8044e95c81401698968ef6e479060cec178206e
Jason Hudgins <jasonleehudgins@gmail.com>
2009-12-27 22:46:34 -0500

    setting up some unit tests for testing the feed parser.

---
+++ tests/src/com/droidworks/syndication/rss/Rss2ParserTest.java
@@ -0,0 +1,53 @@
+package com.droidworks.syndication.rss;
+
+import java.io.InputStream;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.test.InstrumentationTestCase;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Rss2ParserTest extends InstrumentationTestCase {
+
+	AssetManager assetManager;
+	FeedAdapter adapter;
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		assetManager.close();
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		assetManager = getInstrumentation().getContext().getAssets();
+		adapter = new TestFeedAdapter(getInstrumentation().getTargetContext());
+	}
+
+	public void testParser() throws Exception {
+		InputStream feed = assetManager.open("testfeed.txt");
+		assertNotNull(feed);
+
+		Rss2Parser parser = new Rss2Parser(null, adapter);
+		parser.parse(feed);
+
+		assertEquals(60, adapter.getCount());
+	}
+
+	private class TestFeedAdapter extends FeedAdapter {
+
+		public TestFeedAdapter(Context context) {
+			super(context);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// stub
+			return null;
+		}
+
+	}
+
+}
