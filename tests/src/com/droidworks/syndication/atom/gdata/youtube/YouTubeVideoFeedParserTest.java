ef6a2b04f1077d13e89b897fd2e8f43841cd618b
Jason Hudgins <jasonleehudgins@gmail.com>
2009-12-30 22:24:53 -0500

    created a test for the you tube feed parser.

---
+++ tests/src/com/droidworks/syndication/atom/gdata/youtube/YouTubeVideoFeedParserTest.java
@@ -0,0 +1,7 @@
+package com.droidworks.syndication.atom.gdata.youtube;
+
+import android.test.InstrumentationTestCase;
+
+public class YouTubeVideoFeedParserTest extends InstrumentationTestCase {
+
+}
