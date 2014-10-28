c52ef6cff15675a547b9a201dae05371739bd224
Jason Hudgins <jasonleehudgins@gmail.com>
2014-10-27 19:15:42 -0500

    added additional tests

--- tests/src/com/droidworks/syndication/rss/Rss2ParserTest.java
+++ tests/src/com/droidworks/syndication/rss/Rss2ParserTest.java
@@ -12,12 +12,6 @@ public class Rss2ParserTest extends InstrumentationTestCase {
 	AssetManager assetManager;
 
 	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		assetManager.close();
-	}
-
-	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		assetManager = getInstrumentation().getContext().getAssets();
@@ -33,5 +27,19 @@ public class Rss2ParserTest extends InstrumentationTestCase {
 		FeedItem item = parser.getFeed().getItem(0);
 		assertTrue(item.getItunesDuration() > 0);
 	}
+
+
+    public void testParser2() throws Exception {
+
+        InputStream stream = assetManager.open("debugme.rss");
+        assertNotNull(stream);
+
+        Rss2Parser parser = new Rss2Parser();
+        parser.parse(stream);
+        assertEquals(30, parser.getFeed().getItems().size());
+        FeedItem item = parser.getFeed().getItem(0);
+        assertEquals("Re: BOMBSHELL: BIG PHARMA WORLD BANK DOCTORS W/O BORDERS PROOF", item.getTitle());
+    }
+
 
 }
